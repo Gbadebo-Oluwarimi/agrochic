6e33a0f8207b0314482d370a33f3ad5c5509e04e
Gbadebo Oluwarimi <oluwarimigbadebo@gmail.com>
2024-06-17 09:02:35 +0100

    language

--- src/app/[locale]/components/Navbar.js
+++ src/app/[locale]/components/Navbar.js
@@ -94,7 +94,7 @@ const Navbar = ({ locale }) => {
                   <p className="sr-only">change-Language</p>
                   <select
                     defaultValue={localeActive}
-                    className="bg-transparent py-2 cursor-pointer"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     onChange={onSelectChange}
                     disabled={isPending}
                   >
